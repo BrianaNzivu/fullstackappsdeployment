e5f2bb1d2ece081d9a0db083836c5a5811e0a4d8
BrianaNzivu <bjnzivu@gmail.com>
2022-08-29 19:06:09 +0300

    GET and POST requests

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Router, Request, Response } from 'express';
+
 // import bodyParser from 'body-parser'; deprecated
 const bodyParser = require('body-parser')
 
@@ -19,7 +20,7 @@ import { Car, cars as cars_list } from './cars';
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
-    res.status(200).send("Welcome to the Cloud!");
+    res.status(200).send("Hey this is my first server!!!");
   } );
 
   // Get a greeting to a specific person 
@@ -72,11 +73,33 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    // destruct our query paramaters
+    let { make } = req.query;
 
+    
+    let cars_list = cars;
+
+    // if we have an optional query paramater, filter by it
+    if (make) {
+      cars_list = cars.filter((car) => car.make === make);
+    }
+
+    // return the resulting list along with 200 success
+    res.status(200).send(cars_list);
+} 
+);
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
-
+app.get("/cars/:id"), (req: Request, res:Response) => {
+  let{id} = req.params;
+  if(!id) {
+    return res.status(400).send('id is required');
+  }
+  const car = cars.filter((car) => car.id == id);
+  if
+}
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
 
